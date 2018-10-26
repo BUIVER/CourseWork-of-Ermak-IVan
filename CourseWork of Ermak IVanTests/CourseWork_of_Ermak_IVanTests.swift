505e6bd2c8092185feb7fc1003c9f03fca0f2a7d
Ivan <anonumus80@gmail.com>
2018-10-26 12:22:45 +0300

    Initial Commit

---
+++ CourseWork of Ermak IVanTests/CourseWork_of_Ermak_IVanTests.swift
@@ -0,0 +1,34 @@
+//
+//  CourseWork_of_Ermak_IVanTests.swift
+//  CourseWork of Ermak IVanTests
+//
+//  Created by Ivan Ermak on 10/26/18.
+//  Copyright © 2018 Ivan Ermak. All rights reserved.
+//
+
+import XCTest
+@testable import CourseWork_of_Ermak_IVan
+
+class CourseWork_of_Ermak_IVanTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
